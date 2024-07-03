d15d94e424e56b8c9fc6beb6c78a6cbb980405bc
Gareth Murphy <16783958+garethcmurphy@users.noreply.github.com>
2024-07-03 05:17:35 +0200

    x

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title("updated map")
+
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
